7bab51b71a6875c196548d6de10a507bcecb83e6
sonali sharma <sharmasonali3012@gmail.com>
2026-08-18 10:51:34 +0530

    application run

--- app.js
+++ app.js
@@ -5,4 +5,4 @@ app.get('/', (req, res) => {
 res.json({ message: 'Hello from inside Kubernetes!', pod: process.env.HOSTNAME });
 });
 app.get('/health', (req, res) => res.status(200).send('OK'));
-app.listen(PORT, () => console.log(`hello-service listening 
+app.listen(PORT, () => console.log(`hello-service listening`));
